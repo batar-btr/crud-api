bfed97decf163a90d7f271bb32e2a85299ae458b
batar-btr <btrn86@gmail.com>
2023-06-28 17:08:17 +0300

    init: add webpack ts ts-node

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+import path from 'path';
+import url from 'url';
+
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+
+export default {
+  entry: './src/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js']
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  }
+};
